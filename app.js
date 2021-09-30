1e535bbe55531af719ab9ce0cffb5c1be72a670f
NibiXD <c.daniel1505@gmail.com>
2021-09-30 19:12:19 -0300

    Função para carregar lista de despesas no consulta.html e criação de tabelas para os valores inseridos.

--- app.js
+++ app.js
@@ -39,6 +39,25 @@ class Db {
 
         localStorage.setItem('id', id)
     }
+
+    recuperarTodosRegistros() {
+
+        let despesas = new Array();
+
+        let id = localStorage.getItem('id');
+
+        for (let i = 1; i <= id; i++) {
+            let despesa = JSON.parse(localStorage.getItem(i));
+            
+            if (despesa === null) {
+                continue
+            }
+
+            despesas.push(despesa);
+        }
+
+        return despesas 
+    }
 }
 
 let db = new Db();
@@ -56,15 +75,22 @@ function cadastrarDespesa() {
     let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value);
     
     if (despesa.validarDados()) {
+
         document.getElementById('modal-titulo').innerHTML = "Sucesso";
         document.getElementById('modal-titulo-texto').className = "modal-header text-success";
-
         document.getElementById('modal-corpo').innerHTML = 'Despesas incluídas com sucesso!';
-
         document.getElementById('modal-botao').innerHTML = 'Ok';
         document.getElementById('modal-botao').className = 'btn btn-success';
         $('#modalRegistraDespesa').modal('show');
+        
         db.gravar(despesa);
+
+        ano.value = '';
+        mes.value = '';
+        dia.value = '';
+        tipo.value = '';
+        descricao.value = '';
+        valor.value = '';
     } else {
         document.getElementById('modal-titulo').innerHTML = "Erro";
         document.getElementById('modal-titulo-texto').className = "modal-header text-danger";
@@ -76,4 +102,35 @@ function cadastrarDespesa() {
         $('#modalRegistraDespesa').modal('show');
         
     }
+}
+
+function carregaListaDespesas() {
+
+    let despesas = Array();
+    despesas = db.recuperarTodosRegistros();
+
+    let listaDespesas = document.getElementById('listaDespesas');
+    despesas.forEach(function(d) {
+        let lista = listaDespesas.insertRow();
+
+        lista.insertCell(0).innerHTML = `${d.dia}/${d.mes}/${d.ano}`;
+
+        //ajustar o tipo
+        switch(d.tipo){
+            case '1': d.tipo = 'Alimentação';
+                break
+            case '2': d.tipo = 'Educação';
+                break
+            case '3': d.tipo = 'Lazer';
+                break
+            case '4': d.tipo = 'Saúde';
+                break
+            case '5': d.tipo = 'Transporte';
+                break
+        }
+
+        lista.insertCell(1).innerHTML = `${d.tipo}`;
+        lista.insertCell(2).innerHTML = `${d.descricao}`;
+        lista.insertCell(3).innerHTML = `R$${d.valor}`;
+    })
 }
